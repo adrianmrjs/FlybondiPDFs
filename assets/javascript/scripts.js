c0e030bb1e87e4eaa0e1ccea883a01f92ccabcf2
Adrian <adrianrojass86@gmail.com>
2024-02-25 22:07:26 -0300

    second commit

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,10 +1,10 @@
 // Ejemplo de datos de PDFs
 const pdfs = [
-  { name: "ANEXO A", url: "PDFS/PDF1.pdf", category: "anexos" },
-  { name: "ANEXO E", url: "pdfs/pdf2.pdf", category: "procedimientos" },
-  { name: "ANEXO I", url: "PDFS/PDF1.pdf", category: "anexos" },
-  { name: "ANEXO O", url: "pdfs/pdf2.pdf", category: "procedimientos" },
-  { name: "ANEXO U", url: "PDFS/PDF1.pdf", category: "anexos" },
+  { name: "ANEXO A", url: "./assets/PDFS/Ezeiza/ANEXOS/AnexoH.pdf", category: "anexos" },
+  { name: "ANEXO E", url: "pdfs/pdf2.pdf", category: "anexos" },
+  { name: "PDF 2", url: "PDFS/PDF1.pdf", category: "procedimientos" },
+  { name: "PDF 3", url: "pdfs/pdf2.pdf", category: "procedimientos" },
+  { name: "INGRESO", url: "PDFS/PDF1.pdf", category: "procedimientos" },
 ];
 
 // Función para mostrar los resultados de búsqueda filtrados por categoría
